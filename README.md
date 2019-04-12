6dbbddcd0b986a393854d10c718f653e2558d962
cr00z <netrebinr@gmail.com>
2019-04-12 23:24:44 +0700

    write base functional

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Simple Devman API demonstration script.
+
+It determines who sent the tasks to check after midnight.
+
+# How to Install
+
+Python 3 should be already installed. 
+
+Script use [requests](https://pypi.org/project/requests/2.11.1/) and [pytz](https://pypi.org/project/pytz/2019.1/). Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
+
+# Quickstart
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+
+$ python seek_dev_nighters.py
+AntonKoltypin at 11.04.2019 00:15:10
+andreishkilev1993 at 10.04.2019 02:24:25
+andreishkilev1993 at 10.04.2019 02:08:40
+...[skipped]...
+
+```
+
+Use in Windows similarly.
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,41 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+import pytz
+import datetime
 
-def get_midnighters():
-    pass
+
+FIRST_PAGE = 1
+MIDNIGHT = 0
+EARLY_MORNING = 6
+
+
+def get_devman_api_page(page):
+    devman_api = 'https://devman.org/api/challenges/solution_attempts/'
+    return requests.get(devman_api, {'page': page}).json()
+
+
+def load_attempts():
+    num_of_pages = get_devman_api_page(FIRST_PAGE)['number_of_pages']
+    for page in range(1, num_of_pages + 1):
+        user_records = get_devman_api_page(page)['records']
+        for user_record in user_records:
+            yield user_record
+
+
+def get_midnighter_candidate(user):
+    server_time = datetime.datetime.utcfromtimestamp(user['timestamp'])
+    client_time = pytz.timezone(user['timezone']).fromutc(server_time)
+    if MIDNIGHT <= client_time.hour < EARLY_MORNING:
+        return user['username'], client_time
+    else:
+        return None
+
 
 if __name__ == '__main__':
-  pass
+    for user_record in load_attempts():
+        midnighter = get_midnighter_candidate(user_record)
+        if midnighter:
+            midnighter_nick, midnighter_time = midnighter
+            print('{} at {}'.format(
+                midnighter_nick,
+                midnighter_time.strftime('%d.%m.%Y %H:%M:%S')
+            ))
